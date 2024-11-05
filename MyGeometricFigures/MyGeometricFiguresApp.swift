96eed13679680185ec135113f69e7830fd9b741b
Collin Maillet <cmaillet@lcs.on.ca>
2024-11-05 15:19:36 -0500

    Initial Commit

---
+++ MyGeometricFigures/MyGeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGeometricFiguresApp.swift
+//  MyGeometricFigures
+//
+//  Created by Collin Maillet on 2024-11-05.
+//
+
+import SwiftUI
+
+@main
+struct MyGeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
